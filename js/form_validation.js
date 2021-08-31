13b09ab7a379483314ac3fd6f399fa3132f75ec7
Sumesh chandra jena <sumeshjena.co@gmail.com>
2021-09-01 00:56:45 +0530

    Added: Validate name and start date and input display output

--- js/form_validation.js
+++ js/form_validation.js
@@ -1,29 +1,29 @@
-window.addEventListener('DOMContentLoaded',(event)=>{
-    const name=document.querySelector('#name');
-    const textError=document.querySelector('.text-error');
-    name.addEventListener('input',function(){
-        if(name.value.length==0){
-            textError.textContent="";
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function () {
+        if (name.value.length == 0) {
+            textError.textContent = "";
             return
         }
-        try{
-            (new EmployeePayroll()).name=name.value;
-            textError.textContent="";
-        }catch(e){
-            textError.textContent=e;
+        try {
+            (new EmployeePayroll()).name = name.value;
+            textError.textContent = "";
+        } catch (e) {
+            textError.textContent = e;
         }
     });
-    const startdate=document.querySelector('#startdate');
-    const day=document.querySelector('#day');
-    const month=document.querySelector('#month');
-    const year=document.querySelector('#year');
-    const dateError=document.querySelector('.date-error');
-    startdate.addEventListener('input',function(){
-        try{
-           (new EmployeePayroll()).startDate=new Date(Date.UTC(year.value,month.value-1,day.value));
-            dateError.textContent="";
-        } catch(e){
-            dateError.textContent=e;
+    const startdate = document.querySelector('#startdate');
+    const day = document.querySelector('#day');
+    const month = document.querySelector('#month');
+    const year = document.querySelector('#year');
+    const dateError = document.querySelector('.date-error');
+    startdate.addEventListener('input', function () {
+        try {
+            (new EmployeePayroll()).startDate = new Date(Date.UTC(year.value, month.value - 1, day.value));
+            dateError.textContent = "";
+        } catch (e) {
+            dateError.textContent = e;
         }
     })
 
@@ -34,3 +34,46 @@ window.addEventListener('DOMContentLoaded',(event)=>{
         output.textContent = salary.value;
     });
 });
+
+const save = () => {
+    try {
+        let employeePayrollData = createEmployeePayroll();
+    } catch (e) {
+        return;
+    }
+}
+
+const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayroll();
+    try {
+        employeePayrollData.name = getInputValueById('#name');
+    } catch (e) {
+        setTextValue('.text-error', e);
+        throw e;
+    }
+    employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department = getSelectedValues('[name=department]');
+    employeePayrollData.salary = getInputValueById('#salary');
+    employeePayrollData.note = getInputValueById('#notes');
+    employeePayrollData.startDate = new Date(Date.UTC
+        (getInputValueById('#year'), getInputValueById('#month') - 1, getInputValueById('#day')));
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+}
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let selectedItems = [];
+    allItems.forEach(item => {
+        if (item.checked) selectedItems.push(item.value);
+    });
+    return selectedItems;
+}
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+const getInputElementValue = (id) => {
+    let value = document.getElementById(id).value;
+    return value;
+}
